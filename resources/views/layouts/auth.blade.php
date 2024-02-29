a186ce2f93a0cd52b746ce70e3d0b7bfcd024f53
= <muhbintang650@gmail.com>
2024-02-29 13:32:44 +0800

    first commit

---
+++ resources/views/layouts/auth.blade.php
@@ -0,0 +1,53 @@
+@extends('layouts.base')
+
+@section('title', $title)
+
+@section('body-class', 'd-flex flex-column')
+
+@section('content')
+    <div class="page page-center">
+        <div class="container container-normal py-4">
+            <div class="row align-items-center g-4">
+                <div class="col-lg">
+                    <div class="container-tight">
+                        <div class="text-center mb-4">
+                            <a href="." class="navbar-brand navbar-brand-autodark">
+                                <img src="{{ asset('static/logo-light.svg') }}" alt="">
+                            </a>
+                        </div>
+
+                        <div>
+                            @if ($errors->any())
+                                <div class="bg-white alert alert-warning alert-dismissible" role="alert">
+                                    <div class="d-flex ">
+                                        <div class="me-3">
+                                            <h1 class="text-warning las la-exclamation-triangle"></h1>
+                                        </div>
+
+                                        <div>
+                                            <h4 class="alert-title">Ada yang salah.</h4>
+
+                                            <div class="text-muted">
+                                                @foreach ($errors->all() as $error)
+                                                    <p> - {{ $error }}</p>
+                                                @endforeach
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <a class="btn-close" data-bs-dismiss="alert" aria-label="close"></a>
+                                </div>
+                            @endif
+                        </div>
+
+                        {{ $slot }}
+
+                    </div>
+                </div>
+                <div class="col-lg d-none d-lg-block">
+                    <img src="{{ asset('static/illustrations/undraw_secure_login_pdn4.svg') }}" height="300"
+                        class="d-block mx-auto" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
