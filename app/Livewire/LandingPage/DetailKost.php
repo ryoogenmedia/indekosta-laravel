6352665a84b1f4ca60f5f2c6f3490e96070df812
Bintang. <90763575+M-BintangR@users.noreply.github.com>
2024-03-06 00:51:51 +0800

    setup menu landing page 👌

--- app/Livewire/LandingPage/DetailKost.php
+++ app/Livewire/LandingPage/DetailKost.php
@@ -2,10 +2,17 @@
 
 namespace App\Livewire\LandingPage;
 
+
+use Livewire\Attributes\Layout;
+use Livewire\Attributes\Computed;
+use Livewire\Attributes\Title;
 use Livewire\Component;
 
 class DetailKost extends Component
 {
+    #[Layout('layouts.landing-page')]
+    #[Title('Detail Kost')]
+
     public function render()
     {
         return view('livewire.landing-page.detail-kost');
--- app/Livewire/LandingPage/Maps.php
+++ app/Livewire/LandingPage/Maps.php
@@ -2,10 +2,16 @@
 
 namespace App\Livewire\LandingPage;
 
+use Livewire\Attributes\Layout;
+use Livewire\Attributes\Computed;
+use Livewire\Attributes\Title;
 use Livewire\Component;
 
 class Maps extends Component
 {
+    #[Layout('layouts.landing-page')]
+    #[Title('Maps')]
+
     public function render()
     {
         return view('livewire.landing-page.maps');
